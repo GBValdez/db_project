24a3ea4b8ab5be2356817a7053b4c6a54e3c2f12
GBValdez <gabrielbenjaminvaldezdeleon@gmail.com>
2024-10-14 15:40:41 -0600

    Termine la creacion de tablas

--- proyecto.sql
+++ proyecto.sql
@@ -2,10 +2,10 @@ CREATE USER C##project IDENTIFIED BY Contrasena;
 ALTER USER C##LAB1 QUOTA UNLIMITED ON USERS;
 GRANT DBA TO "C##PROJECT";
 
-CREATE OR REPLACE PROCEDURE addBinnacleData(tableName IN VARCHAR2) AS
-    queryData VARCHAR2(1000);
+CREATE OR REPLACE PROCEDURE pro_add_binnacle_data(tableName IN VARCHAR2) AS
+    query_data VARCHAR2(1000);
 BEGIN
-	queryData := 'ALTER TABLE ' || tableName || 
+	query_data := 'ALTER TABLE ' || tableName || 
                 ' ADD ( create_at DATE,'||
 				' create_user_id INT, ' ||
                 ' update_at DATE, ' ||
@@ -13,17 +13,17 @@ BEGIN
                 ' delete_at DATE)';
 	DBMS_OUTPUT.PUT_LINE('Columnas agregadas a la tabla ' || tableName);
                 
-    EXECUTE IMMEDIATE queryData;
+    EXECUTE IMMEDIATE query_data;
 	
-    queryData := 'ALTER TABLE ' || tableName || 
+    query_data := 'ALTER TABLE ' || tableName || 
                 ' ADD  CONSTRAINT ' || tableName || '_user_create_id FOREIGN KEY (CREATE_USER_ID) REFERENCES USERS(id)';
 
-	EXECUTE IMMEDIATE queryData;
+	EXECUTE IMMEDIATE query_data;
 
-	queryData := 'ALTER TABLE ' || tableName || 
+	query_data := 'ALTER TABLE ' || tableName || 
                 ' ADD  CONSTRAINT ' || tableName || '_user_update_id FOREIGN KEY (UPDATE_USER_ID) REFERENCES USERS(id)';
 
-    EXECUTE IMMEDIATE queryData;
+    EXECUTE IMMEDIATE query_data;
     -- Mensaje opcional de éxito
     DBMS_OUTPUT.PUT_LINE('Columna userId y clave foránea agregadas a la tabla ' || tableName);
     
@@ -51,7 +51,7 @@ CREATE TABLE users(
 	CONSTRAINT users_rol_id FOREIGN KEY (rol_id) REFERENCES rol(id)	
 );
 
-CREATE TABLE blood_Type(
+CREATE TABLE blood_type(
 	id int PRIMARY KEY ,
 	name varchar2(5) NOT NULL ,
 	description varchar2(100)
@@ -119,10 +119,81 @@ CREATE TABLE patient(
 	sex_id int NOT NULL,
 	blood_Type_id int NOT NULL,
 	constraint patient_sex_id foreign key (sex_id) references sex (id),
-	constraint patient_speciality_id foreign key (speciality_id) references specialty(id)
+	constraint patient_blood_Type_id foreign key (blood_Type_id) references blood_type(id)
+);
+
+CREATE TABLE consultation(
+	id int PRIMARY KEY,
+	date_visit DATE NOT NULL,
+	general_comments varchar2(100),
+	doctor_cui varchar2(13) NOT null,
+	patient_cui varchar2(13) NOT NULL,
+	presc_status_id int NOT NULL,
+	CONSTRAINT colsutation_doctor_id FOREIGN KEY (doctor_cui) REFERENCES doctor(cui),
+	CONSTRAINT colsutation_patient_id FOREIGN KEY (patient_cui) REFERENCES patient(cui),
+	CONSTRAINT colsutation_presc_status_id FOREIGN KEY (presc_status_id) REFERENCES presc_status(id)
+);
+
+CREATE TABLE diseases(
+	id int PRIMARY KEY,
+	name varchar2(30) NOT NULL,
+	description varchar2(200) NOT NULL,
+	sintomas varchar2(300) NOT NULL,
+	classification_id int NOT NULL,
+	CONSTRAINT disease_dis_class_id FOREIGN KEY (classification_id) REFERENCES dis_class(id)
+);
+
+CREATE TABLE pharma_form_dose_unit(
+	id int PRIMARY KEY,
+	pharma_form_id int NOT NULL,
+	dose_unit_id int NOT NULL,
+	CONSTRAINT pfdu_pharma_form_id FOREIGN KEY (pharma_form_id) REFERENCES pharma_form(id), 
+	CONSTRAINT pfdu_dose_unit_id FOREIGN KEY (dose_unit_id) REFERENCES dose_unit(id)
+);
+
+CREATE TABLE medicine(
+	id int PRIMARY KEY,
+	name varchar2(30) NOT NULL,
+	brand_id int NOT NULL,
+	description varchar2(200) NOT NULL,
+	composition varchar2(200) NOT NULL,
+	pfdu_id int NOT NULL,
+	recommended_amount float NOT NULL,
+	indications varchar2(200) NOT NULL,
+	adverse_effects varchar2(200) NOT NULL,
+	CONSTRAINT medicine_brand_id FOREIGN KEY (brand_id) REFERENCES med_brand(id),
+	CONSTRAINT medicine_pfdu_id FOREIGN KEY (pfdu_id) REFERENCES pharma_form_dose_unit(id) 
+);
+
+CREATE TABLE diseases_medicine (
+	disease_id int NOT NULL,
+	medicine_id int NOT NULL,
+	recommended_dosage float NOT null,
+	CONSTRAINT dm_disease_id FOREIGN KEY (disease_id) REFERENCES diseases(id),
+	CONSTRAINT dm_medicine_id FOREIGN KEY (medicine_id) REFERENCES medicine(id)
+);
+
+CREATE TABLE diagnosed_disease(
+	id int PRIMARY KEY,
+	consultation_id int NOT NULL,
+	disease_id int NOT NULL,
+	CONSTRAINT dd_consultation_id FOREIGN KEY (consultation_id) REFERENCES consultation(id),
+	CONSTRAINT dd_disease_id FOREIGN KEY (disease_id) REFERENCES diseases(id)
+);
+
+CREATE TABLE recommended_medication(
+	id int PRIMARY KEY,
+	diagnosed_disease_id int NOT NULL,
+	dose float NOT NULL,
+	frequency_hrs float NOT NULL,
+	duration_days float,
+	additional_instructions varchar2(200),
+	medicine_id int NOT NULL,
+	CONSTRAINT rec_med_diagnosed_disease_id FOREIGN KEY (diagnosed_disease_id) REFERENCES diagnosed_disease(id),
+	CONSTRAINT rec_med_medicine_id FOREIGN KEY (medicine_id) REFERENCES medicine(id)
 );
 
 BEGIN 
-	addBinnacleData('users');
+	pro_add_binnacle_data('users');
 	addBinnacleData('rol');
 END;
